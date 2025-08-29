4fa61b4946acc5db7d2e4095286c6c1954eaf402
cintyaviana <cintyhelenvc@outlook.com>
2025-08-29 20:14:36 -0300

    Dados Diários

--- CursoPythonFarmax/FundamentosPython/Aula-3/Aula3.py
+++ CursoPythonFarmax/FundamentosPython/Aula-3/Aula3.py
@@ -1,61 +1,61 @@
 
 # Comando IF
 
-numero = int(input('Forneça um valor inteiro'))
-if numero > 0:
-    print('O número informado é positivo')
-    print("Segunda instrução do bloco")
+# numero = int(input('Forneça um valor inteiro'))
+# if numero > 0:
+#     print('O número informado é positivo')
+#     print("Segunda instrução do bloco")
 
-print()
-print("------------------------------------------------------")
-print()
+# print()
+# print("------------------------------------------------------")
+# print()
 
-numero = int(input('Forneça um valor inteiro: '))
-if numero > 0:
-    print('O número informado é positivo')
-print("Segunda instrução do bloco")
+# numero = int(input('Forneça um valor inteiro: '))
+# if numero > 0:
+#     print('O número informado é positivo')
+# print("Segunda instrução do bloco")
 
-print()
-print("------------------------------------------------------")
-print()
+# print()
+# print("------------------------------------------------------")
+# print()
 
 # Comando If ... Else
 
-numero = int(input('Forneça um valor inteiro: '))
+# numero = int(input('Forneça um valor inteiro: '))
 
-if numero % 2 == 0:
-    print('Voce informou um número par.')
-    print('Numero informado:', numero)
-else:
-    print('Voce informou um número ímpar.')
-    if numero > 10:
-        print('O número é maior que 10.')
-    else:
-        print('O número não é maior que 10.')
+# if numero % 2 == 0:
+#     print('Voce informou um número par.')
+#     print('Numero informado:', numero)
+# else:
+#     print('Voce informou um número ímpar.')
+#     if numero > 10:
+#         print('O número é maior que 10.')
+#     else:
+#         print('O número não é maior que 10.')
 
-print()
-print("------------------------------------------------------")
-print()
+# print()
+# print("------------------------------------------------------")
+# print()
 
 # Comando if...elif...else
 
-nome = input('Nome do associado: ')
-idade = int(input('Idade do Associado: '))
+# nome = input('Nome do associado: ')
+# idade = int(input('Idade do Associado: '))
 
 
-if idade < 18:
-    ingresso = 50
-elif idade >= 60:
-    ingresso = 20
-else:
-    ingresso = 60
+# if idade < 18:
+#     ingresso = 50
+# elif idade >= 60:
+#     ingresso = 20
+# else:
+#     ingresso = 60
 
-print("Associoado:", nome)
-print("Valor a ser pago:", ingresso)
+# print("Associoado:", nome)
+# print("Valor a ser pago:", ingresso)
 
-print()
-print("------------------------------------------------------")
-print()
+# print()
+# print("------------------------------------------------------")
+# print()
 
 # Listas - conjunto de valores, definidos entre colchetes
 
@@ -69,12 +69,12 @@ mistos = ['São Paulo', 322, True, 75.2]
 
 """ Para buscar um elemento em uma lista pode-se usar índices com valores positivos ou negtivos. Se o índice for positivo, a localização é realizado a partir do zero para a direira, e se for negativo, para a esquerda"""
 
-print(nomes)
-print(type(nomes))
-print(nomes[0])               # primeiro elemento
-print(nomes[-1])             # último elemento
-print(nomes[-2])             # penúltimo elemento
-print(nomes[len(nomes)-1])   # último elemento
+# print(nomes)
+# print(type(nomes))
+# print(nomes[0])               # primeiro elemento
+# print(nomes[-1])             # último elemento
+# print(nomes[-2])             # penúltimo elemento
+# print(nomes[len(nomes)-1])   # último elemento
 
 """ (len(nomes): A função len() retorna o número total de elementos na lista nomes. Por exemplo, se a lista tem 5 elementos, len(nomes) retornará 5, nesse caso, 5-1 = 4, ou seja posição 4 é o último elemento dessa lista"""
 
@@ -85,9 +85,25 @@ print(nomes[len(nomes)-1])   # último elemento
 A aplicação do operador slice consiste na utilização dos limites numéricos separados por : (dois pontos).
 
 [posição_inicial: limite]
+
+numeros = [5.5, 9, 14, 16, 1, 8]
+Posição =  0,   1,  2,  3, 4, 5
 """
 
+# print(type(numeros))
+# print(numeros)          # Retorna todos as posiçõe da lista
+# print(numeros[2:])      # Retorna os elementos da posição 2 até o final
+# print(numeros[2:4])     # Retorna os elementos da posição 2 até a posição 3, pois o 4 é excludente
+# print(numeros[2:8])     # Retorna os elementos da posição 2 até o final, pois tem mmenos que 8 posições na lista
+# print(numeros[:5])      # Retorna os elementos da posição inicial até a 4
+
+
+# Troca de elementos de uma lista
+
 print(numeros)
-print(numeros[2:0])
-print(numeros[2:4])
-print(numeros[2:0])
+temp = numeros[1]
+print(temp)
+numeros[1] = numeros[3]   # numeros = [1] = 9, troca por numeros = [3] = 16
+print(numeros)
+numeros[3] = temp
+print(numeros)
